42935d712d56c6ebe51c9843ae94cf741748a1ec
Evan Fleming <evan.gordon.fleming@gmail.com>
2021-09-11 14:55:23 -0700

    first try making an nft smart contract

---
+++ my-nft/scripts/mint-nft.js
@@ -0,0 +1,32 @@
+const env = require("dotenv");
+const { ethers } = require("ethers");
+const { abi } = require("../artifacts/contracts/my-nft.sol/MyNFT.json");
+const nft = require("../nfts/dog.json");
+
+env.config();
+const { PUBLIC_KEY, CONTRACT_ADDRESS, API_ID, PRIVATE_KEY } = process.env;
+
+const provider = new ethers.providers.InfuraProvider("rinkeby", API_ID);
+
+const wallet = new ethers.Wallet(PRIVATE_KEY);
+const signer = wallet.connect(provider);
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+const exec = async () => {
+  const tx = await contract.mintNFT(
+    PUBLIC_KEY,
+    "https://gateway.pinata.cloud/ipfs/QmarPqdEuzh5RsWpyH2hZ3qSXBCzC5RyK3ZHnFkAsk7u2f"
+  );
+  await tx.wait();
+  console.log(`NFT Minted! Tx: ${tx.hash}`);
+};
+
+exec()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
